2b0e197f99342a381c8a8aae61a363876652e661
budzikt <budzikt@gmail.com>
2016-01-10 12:25:13 +0100

    marked styles + hoover menu

--- Code/aligner.js
+++ Code/aligner.js
@@ -4,17 +4,27 @@ $( document ).ready(function() {
 	var CheckBoxClass = 'RemarkCheckBox';	
 	var CheckBoxString ='<td>'
 						+'<form action="" method="get">'
-  						+'<input type="checkbox" name="vehicle" value="Bike">Remarks<br>'
+  						+'<input type="checkbox" name="RemarkBox" value="Remark">Remarks<br>'
 						+'</form></td>';	
     $('tr').prepend(CheckBoxString);
     $('tr td:first-child').addClass(CheckBoxClass);
     
+    //This is just bad... but works.
+    // TODO howto obtain ref to newly created element without referencing to parent
+    var tableRef = $('table').before('<div></div>');
+    tableRef.prev('div').addClass('hoovering');
+    
     
     $(function(){
     $("tr td.RemarkCheckBox input").click(function(event) {
-    	if(event.currentTarget.checked == true){
-  			alert(event.pageX);
-       		alert(event.pageY);  		
+    	
+    	var par = $(event.target).closest( "tr" );
+    	
+    	if(event.target.checked == true){
+       		par.addClass("marked"); 			
+    	}
+    	else{
+    		par.removeClass("marked"); 
     	}
 
     });
