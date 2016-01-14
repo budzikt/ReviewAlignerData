17e799087ddfdf0e5f4f818b9c7a0cbad8d0fdd5
budzikt <budzikt@gmail.com>
2016-01-14 21:58:05 +0100

    Majkr simplifications

--- Code/aligner.js
+++ Code/aligner.js
@@ -1,11 +1,11 @@
 
 $( document ).ready(function() {
 
-	var CheckBoxClass = 'RemarkCheckBox';	
+	var CheckBoxClass = 'RemarkTd';	
 	var CheckBoxString ='<td>'
 						+'<form action="" method="get">'
-  						+'<input type="checkbox" name="RemarkBox" class="shown remarkCheck">'
-  						+'<input type="text" name="remarkText" class="hidden remarkText">'
+  						+'<input type="checkbox" 	class="remarkCheckBox">'
+  						+'<input type="text" 		class="remarkText">'
 						+'</form></td>';
 						
 	var FormCode = '<form method="POST"><input type="submit" value="Submit"></form>';
@@ -13,36 +13,24 @@ $( document ).ready(function() {
     $('tr').prepend(CheckBoxString);
     $('tr td:first-child').addClass(CheckBoxClass);
     
-    //This is just bad... but works.
-    // TODO howto obtain ref to newly created element without referencing to parent
-    var tableRef = $('table').before('<div></div>');
-    tableRef.prev('div').addClass('hoovMenu');
-    tableRef.prev('div.hoovMenu').append(FormCode);
-    
-    
-    
+    // Select table element, embed div before, select those div, add hover class to it and append Sign Form code.
+    //One horrible line. <;_;>
+    var tableRef = $('table').before('<div></div>').prev('div').addClass('hoovMenu').append(FormCode);
+    //Hide all textboxes
+    $('tr td.RemarkTd input.remarkText').hide(0);
+   
     $(function(){
-    $("tr td.RemarkCheckBox input.remarkCheck").click(function(event) {
+    $("tr td.RemarkTd input.remarkCheckBox").click(function(event) {
     	
     	var checkedTr = $(event.target).closest( "tr" );
     	var commentBox = checkedTr.find('.remarkText');
     	
     	if(event.target.checked == true){
        		checkedTr.addClass("marked");
-       		
-       		if(commentBox.hasClass('hidden')) 
-       		{commentBox.addClass('shown').removeClass('hidden');}
-       		else 
-       		{commentBox.addClass('shown');}
        		$(commentBox).show(400);
     	}
     	else{
     		checkedTr.removeClass("marked");
-    		
-    		if(commentBox.hasClass('shown')) 
-    		{commentBox.addClass('hidden').removeClass('shown');}
-       		else 
-       		{commentBox.addClass('hidden');}
        		$(commentBox).hide(400);
     	}
 
